8002a1b0751b8227121812a0ce734d6c2d2db34f
Vitalii57 <vitalij.zubkov.2017@inbox.ru>
2023-05-11 17:55:31 +0300

    buttonTwo

--- Untitled-2.js
+++ Untitled-2.js
@@ -4,7 +4,7 @@ const phone_container = document.querySelector(".container_phone");
 import { phones } from "./data.js";
 phones.forEach(function (phone) {
   const listItem1 = template.content.firstElementChild.cloneNode(true);
-  listItem1.querySelector("img").src = phone.img
+  listItem1.querySelector("img").src = phone.img;
   listItem1.querySelector(".body__text__sale").innerText = phone.discount;
   listItem1.querySelector(".body-card__title-link").innerText = phone.name;
   listItem1.querySelector(".price1").innerText = phone.price1;
@@ -26,7 +26,7 @@ button.onclick = function filter() {
   phone_container.innerHTML = "";
   filteredPhones.forEach(function (phone) {
     const listItem1 = template.content.firstElementChild.cloneNode(true);
-    listItem1.querySelector("img").src = phone.img
+    listItem1.querySelector("img").src = phone.img;
     listItem1.querySelector(".body__text__sale").innerText = phone.discount;
     listItem1.querySelector(".body-card__title-link").innerText = phone.name;
     listItem1.querySelector(".price1").innerText = phone.price1;
@@ -35,5 +35,26 @@ button.onclick = function filter() {
     listItem1.querySelector(".search");
     phone_container.append(listItem1);
   });
-  console.log(filteredPhones)
+  console.log(filteredPhones);
+};
+
+buttonTwo.onclick = function filter() {
+  const filterValue = input.value;
+  const filterByName = (arrayElement) => {
+    return arrayElement.name.includes(filterValue);
+  };
+  const filteredPhones = phones.filter(filterByName);
+  phone_container.innerHTML = "";
+  filteredPhones.forEach(function (phone) {
+    const listItem1 = template.content.firstElementChild.cloneNode(true);
+    listItem1.querySelector("img").src = phone.img;
+    listItem1.querySelector(".body__text__sale").innerText = phone.discount;
+    listItem1.querySelector(".body-card__title-link").innerText = phone.name;
+    listItem1.querySelector(".price1").innerText = phone.price1;
+    listItem1.querySelector(".price2").innerText = phone.price2;
+    listItem1.querySelector(".button").innerText = phone.button;
+    listItem1.querySelector(".search");
+    phone_container.append(listItem1);
+  });
+  console.log(filteredPhones);
 };
